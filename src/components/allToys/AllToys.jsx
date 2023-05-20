2fd1075898246e63af3236f263d79b4c46c3de60
sourovmodak06 <sourovmodak650@gmail.com>
2023-05-21 02:13:15 +0600

    results limit section added

--- src/components/allToys/AllToys.jsx
+++ src/components/allToys/AllToys.jsx
@@ -1,11 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import useTitle from "../shared/hooks/useTitle";
 import { useLoaderData } from "react-router-dom";
 import TableInfo from "./TableInfo";
+import { FaCaretLeft, FaCaretRight } from "react-icons/fa";
 
 const AllToys = () => {
   useTitle("All Toy");
   const toys = useLoaderData();
+
+  const [currentPage,setCurrentPage] = useState(1);
+  const recordsPerPage = 20;
+  const lastIndex = currentPage * recordsPerPage;
+  const firstIndex = lastIndex - recordsPerPage;
+  const records = toys.slice(firstIndex,lastIndex);
+  const npage = Math.ceil(toys.length / recordsPerPage);
+  const number = [...Array(npage + 1).keys()].slice(1);
+
+
+  function perPage() {
+    if (currentPage !== 1) {
+      setCurrentPage(currentPage - 1);
+    }
+  }
+  function changeCPage(id) {
+    setCurrentPage(id);
+  }
+  function nextPage() {
+    if (currentPage !== npage) {
+      setCurrentPage(currentPage + 1);
+    }
+  }
+  
   return (
     <div className="px-10">
       <div className="overflow-x-auto w-full">
@@ -21,11 +46,28 @@ const AllToys = () => {
             </tr>
           </thead>
           <tbody>
-            {toys.map((toy) => (
+            {records.map((toy) => (
               <TableInfo key={toy._id} toy={toy} ></TableInfo>
             ))}
           </tbody>
         </table>
+      </div>
+      <div className="flex justify-center py-2">
+        <ul className="flex items-center gap-4">
+          <li className="btn">
+            <button onClick={perPage}><FaCaretLeft></FaCaretLeft></button>
+          </li>
+          {
+            number.map((n,i) => (
+              <li key={i} className={`${currentPage === n ? 'bg-black text-white p-2 rounded-full' : ''}`}>
+                <button onClick={() => changeCPage(n)}>{n}</button>
+              </li>
+            ))
+          }
+          <li className="btn">
+            <button onClick={nextPage}><FaCaretRight></FaCaretRight></button>
+          </li>
+        </ul>
       </div>
     </div>
   );
